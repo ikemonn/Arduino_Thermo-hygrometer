7ea94d7762521bf23e9f4935316730faea2637f2
ikemoto <ikemoto.daiki@gmail.com>
2016-02-14 09:48:12 +0900

    send datadog

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var app = express();
 
+var DataDog = require('./node-datadog');
+var api_key = "your api key";
+var app_key = "your app key";
+var dd = new DataDog(api_key, app_key);
+
 app.get('/', function (req, res) {
     console.log(req.query);
+    dd.postSeries({
+        "series": [
+            {
+                "metric": "arduino.temperature",
+                "points": [
+                    [Date.now()/1000, req.query['t']]
+                ],
+                "type": "gauge",
+                "tags": ["arduino", "temperature"]
+            },
+            {
+                "metric": "arduino.humidity",
+                "points": [
+                    [Date.now()/1000, req.query['h']]
+                ],
+                "type": "gauge",
+                "tags": ["arduino", "humidity"]
+            }
+        ]
+    });
+
 });
 
 app.listen(3000, function () {
